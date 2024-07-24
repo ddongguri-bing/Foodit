46c711671c690975a00d22a500134ba3dea4b5df
ddongguri-bing <ckdalssnsk@naver.com>
2024-07-24 15:03:13 +0900

    Foodit: 입력 폼(2)

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -2,35 +2,49 @@ import { useState } from 'react';
 import './FoodForm.css';
 
 function FoodForm() {
-  const [title, setTitle] = useState('');
-  const [calorie, setCalorie] = useState(0);
-  const [content, setContent] = useState('');
+  const [values, setValues] = useState({
+    title: '',
+    calorie: 0,
+    content: '',
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  function sanitize(type, value) {
+    // calorie 값이 숫자로 처리되지 않는 문제 해결을 위한 함수
+    switch (type) {
+      case 'number':
+        return Number(value) || 0;
+
+      default:
+        return value;
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value, type } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: sanitize(type, value),
+    }));
   };
 
-  const handleCalorieChange = (e) => {
-    const nextCalorie = Number(e.target.value) || 0;
-    setCalorie(nextCalorie);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
 
   return (
-    <form className="FoodForm">
-      <input name="title" value={title} onChange={handleTitleChange}></input>
+    <form className="FoodForm" onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange}></input>
       <input
         name="calorie"
         type="number"
-        value={calorie}
-        onChange={handleCalorieChange}></input>
+        value={values.calorie}
+        onChange={handleChange}></input>
       <input
         name="content"
-        value={content}
-        onChange={handleContentChange}></input>
+        value={values.content}
+        onChange={handleChange}></input>
+      <button type="submit">확인</button>
     </form>
   );
 }
