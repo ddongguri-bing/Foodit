dc30291361460773c3e008b6d4a93d53bc8f30bf
ddongguri-bing <ckdalssnsk@naver.com>
2024-07-24 16:07:28 +0900

    Foodit: 파일 인풋

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,49 +1,61 @@
 import { useState } from 'react';
-import './FoodForm.css';
+import FileInput from './FileInput';
+// import './FoodForm.css';
+
+function sanitize(type, value) {
+  switch (type) {
+    case 'number':
+      return Number(value) || 0;
+
+    default:
+      return value;
+  }
+}
 
 function FoodForm() {
   const [values, setValues] = useState({
     title: '',
     calorie: 0,
     content: '',
+    imgFile: null,
   });
-
-  function sanitize(type, value) {
-    // calorie 값이 숫자로 처리되지 않는 문제 해결을 위한 함수
-    switch (type) {
-      case 'number':
-        return Number(value) || 0;
-
-      default:
-        return value;
-    }
-  }
-
-  const handleChange = (e) => {
-    const { name, value, type } = e.target;
-    setValues((prevValues) => ({
-      ...prevValues,
-      [name]: sanitize(type, value),
-    }));
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
   };
 
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    handleChange(name, sanitize(type, value));
+  };
+
   return (
-    <form className="FoodForm" onSubmit={handleSubmit}>
-      <input name="title" value={values.title} onChange={handleChange}></input>
+    <form onSubmit={handleSubmit}>
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        onChange={handleChange}
+      />
+      <input name="title" value={values.title} onChange={handleInputChange} />
       <input
-        name="calorie"
         type="number"
+        name="calorie"
         value={values.calorie}
-        onChange={handleChange}></input>
+        onChange={handleInputChange}
+      />
       <input
         name="content"
         value={values.content}
-        onChange={handleChange}></input>
+        onChange={handleInputChange}
+      />
       <button type="submit">확인</button>
     </form>
   );
